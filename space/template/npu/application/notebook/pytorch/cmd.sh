15dd7b2cfe127551b2fa5680c3613289e6b95d14
wangli <858794774@qq.com>
2024-09-27 14:58:36 +0800

    modify dir level

---
+++ space/template/npu/application/notebook/pytorch/cmd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+jupyter-lab \
+    --ip 0.0.0.0 \
+    --port 7860 \
+    --no-browser \
+    --allow-root \
+    --ServerApp.allow_origin='*' \
+    --ServerApp.base_url="$GRADIO_ROOT_PATH" \
+    --ServerApp.token="$JUPYTER_TOKEN" \
+    --ServerApp.tornado_settings="{'headers': {'Content-Security-Policy': 'frame-ancestors *'}}" \
+    --ServerApp.cookie_options="{'SameSite': 'None', 'Secure': True}" \
+    --ServerApp.disable_check_xsrf=True \
+    --LabApp.news_url=None \
+    --LabApp.check_for_updates_class="jupyterlab.NeverCheckForUpdate" \
+    --notebook-dir=$NOTEBOOK_DIR
